ffea627d5c873e1d187360700027595e14eacba5
Giulia Cardieri <giulia.cardieri@gmail.com>
2018-11-11 22:50:55 -0200

    Remove unused files, update service worker

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,31 @@
-let version = 1.0;
-
-let cacheName = "MyCacheV" + version;
+let cacheName = "pareionde-cache-v0";
 
 let filesToCache = [
-	"/",
-	"/index.html",
-	"js/main.js",
-	"css/main.js"
+	"index.html",
+	"css/style.css",
+  "css/modulos/variaveis.css",
+  "css/libraries/all.min.css",
+  "css/libraries/bootstrap.min.css",
+  "css/libraries/bootstrap.min.css.map",
+  "css/elementos/accordion.css",
+  "css/elementos/add-form.css",
+  "css/elementos/buttons.css",
+  "css/elementos/details.css",
+  "css/elementos/footer.css",
+  "css/elementos/header.css",
+  "css/elementos/main.css",
+  "js/db.js",
+  "js/details.js",
+  "js/form.js",
+  "js/navigation.js",
+  "js/window.js",
+  "js/pwa-eu/conexao.js",
+  "js/pwa-eu/gerenciamento.js",
+  "js/libraries/bootstrap.min.js",
+  "js/libraries/bootstrap.min.js.map",
+  "js/libraries/handlebars.js",
+  "js/libraries/jquery.min.js",
+  "js/libraries/popper.min.js"
 ];
 
 self.addEventListener("install", function(event) {
